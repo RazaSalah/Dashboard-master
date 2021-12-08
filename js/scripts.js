f470e3185c4f5b280efefde7e2bc0bdf2c99791d
RazaSalah <razansalah41@gmail.com>
2021-12-09 01:21:57 +0300

    add

--- js/scripts.js
+++ js/scripts.js
@@ -4,3 +4,17 @@
     const sidebarToggle = document.body.querySelector('#sidebarToggle');
 
     
+// i have to run the file with the live server so it will work
+    $(document).ready(function() {
+        $('#Customer').DataTable( {
+            "ajax": "./json/data.json",
+            "columns": [
+                { "data": "name" },
+                { "data": "address" },
+                { "data": "city" },
+                { "data": "age" },
+                { "data": "last visit" },
+                { "data": "orders" }
+            ]
+        } );
+    } );
