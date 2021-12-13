1887aff974e3383b225957985150961152e4b2ca
RazaSalah <razansalah41@gmail.com>
2021-12-13 23:54:02 +0300

    add

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,35 @@
-// import './products.js';
-    
-    // Toggle the side navigation  
-    // const sidebarToggle = document.body.querySelector('#wrapper');
-        var el = document.getElementById("wrapper");
-        var toggleButton = document.getElementById("menu-toggle");
-        
-        toggleButton.onclick = function () {
-            el.classList.toggle("toggled");
-        };
 
 
-    
+$(function () {
+    $("#header").load("header.html");
+  });
+$(function () {
+  $("#sidebar-wrapper").load("sidebar.html");
+});
+$(function () {
+  $("#footer").load("footer.html");
+});
+
+// Toggle the side navigation
+// const sidebarToggle = document.body.querySelector('#wrapper');
+var el = document.getElementById("wrapper");
+var toggleButton = document.getElementById("menu-toggle");
+
+toggleButton.onclick = function () {
+  el.classList.toggle("toggled");
+};
+
 // i have to run the file with the live server so it will work
-    $(document).ready(function() {
-        $('#Customer').DataTable( {
-            "ajax": "./json/data.json",
-            "columns": [
-                { "data": "name" },
-                { "data": "address" },
-                { "data": "city" },
-                { "data": "age" },
-                { "data": "last visit" },
-                { "data": "orders" }
-            ]
-        } );
-    } );
-
-
+$(document).ready(function () {
+  $("#Customer").DataTable({
+    ajax: "./json/data.json",
+    columns: [
+      { data: "name" },
+      { data: "address" },
+      { data: "city" },
+      { data: "age" },
+      { data: "last visit" },
+      { data: "orders" },
+    ],
+  });
+});
